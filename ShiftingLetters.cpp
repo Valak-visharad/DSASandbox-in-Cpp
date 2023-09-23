647b92f300b5c7bb3e3f11c7f34525434e60cca7
Enigma <vilakshanjoshi7778@hotmail.com>
2023-09-23 19:29:01 +0530

    Create ShiftingLetters.cpp

---
+++ ShiftingLetters.cpp
@@ -0,0 +1,27 @@
+// hue hue hue
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.length();
+        long long int arr[n];
+        long long sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += shifts[i];
+        for (int i = 0; i < n; i++) {
+            arr[i] = int(s[i]) + sum;
+            sum -= shifts[i];
+            arr[i] = (arr[i] - 97) % 26 + 97;
+        }
+        string update = string(1, char(arr[0])); // Initialize the update string with the first character
+        for (int i = 1; i < n; i++)
+            update += char(arr[i]); // Append characters to the update string
+        return update;
+    }
+};
+
