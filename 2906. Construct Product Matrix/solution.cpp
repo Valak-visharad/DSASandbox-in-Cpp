df02204d11c792c0ce4e5459bf3513edcf56d47e
Valak <59192647+Valak-visharad@users.noreply.github.com>
2023-10-26 22:18:25 +0530

    hue hue

--- 2906. Construct Product Matrix/solution.cpp
+++ 2906. Construct Product Matrix/solution.cpp
@@ -1,29 +1,76 @@
 // https://leetcode.com/problems/construct-product-matrix/
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+    vector<vector<int>> constructProductMatrix(vector<vector<int>> &grid)
+    {
         int n = grid[0].size();
         int m = grid.size();
+        // cout << n << m;
 
-        cout << n << m;
+        vector<vector<int>> p(n, vector<int>(m, 1));
 
-        return grid;
+        // // cout << "p =>\n{";
+        // for (int i = 0; i < n; i++)
+        // {
+        //     // cout << "{ ";
+        //     for (int j = 0; j < m; j++)
+        //     {
+        //         // cout << p[i][j] << " ";
+        //     }
+        //     // cout << " }\n";
+        // }
+        // // cout << "\n}";
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                // cout << "p[" << i << "][" << j << "] = " << p[i][j] << endl;
+                for (int k = 0; k < n; k++)
+                {
+                    for (int l = 0; l < m; l++)
+                    {
+                        if (i != k || j != l)
+                        {
+                            p[i][j] *= grid[k][l];
+                        }
+                    }
+                }
+                // cout << "p[" << i << "][" << j << "] = " << p[i][j] << endl;
+            }
+        }
+
+
+        // // cout << "p =>\n{";
+        // for (int i = 0; i < n; i++)
+        // {
+        //     // cout << "{ ";
+        //     for (int j = 0; j < m; j++)
+        //     {
+        //         // cout << p[i][j] << " ";
+        //     }
+        //     // cout << " }\n";
+        // }
+        // // cout << "\n}";
+
+
+        return p;
     }
 };
-
 
 
 int main()
 {
     Solution sol;
 
-    sol.constructProductMatrix({{1, 2}, {3, 4}});
+    vector<vector<int>> grid = {{1, 2}, {3, 4}};
+    sol.constructProductMatrix(grid);
 
     return 0;
 }
