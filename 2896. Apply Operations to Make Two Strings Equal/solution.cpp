b260ab5a194cf1760311997a54b06e64068293f4
Valak <59192647+Valak-visharad@users.noreply.github.com>
2023-11-09 22:20:54 +0530

    day-2

--- 2896. Apply Operations to Make Two Strings Equal/solution.cpp
+++ 2896. Apply Operations to Make Two Strings Equal/solution.cpp
@@ -1,20 +1,22 @@
 // 2896. Apply Operations to Make Two Strings Equal
+#include<iostream>
+using namespace std;
+
 
 class Solution {
 public:
     int minOperations(string s1, string s2, int x) {
-        int s1_one = 0, s2_one = 0;
-        int n = sizeof(s1);
+        int diff = 0;
+        int n = s1.size();
         int prevIndex = 0, accounted = 0;
         int cost = 0;
 
+        cout << "inside the function\n";
+
         for (int i = 0; i < n; i++)
         {
-            if (s1[i] == '1')
-                s1_one++;
-            if (s2[i] == '1')
-                s2_one++;
-            
+            diff += abs(s1[i] - s2[i]);
+            cout << "(s1[i] != s2[i])" << s1[i] << "\t" << s2[i] << "\t" << (s1[i] != s2[i]) << endl;
             if (s1[i] != s2[i])
             {
                 if (accounted)
@@ -29,12 +31,13 @@ public:
                 }
             }
         }
-        if (s1_one == s2_one)
-            return cost;
+        cout << "after the for loop";
 
-        cout << s1_one << s2_one; // DEBUG CODE
+        diff = diff % 2;
+        if (diff) // diff is odd
+            return -1;
 
-        return -1;
+        return cost; // if diff is even
     }
 };
 
@@ -43,5 +46,11 @@ public:
 int main()
 {
     Solution s;
-    s.minOperations()
+    string s1 = "1100011000";
+    string s2 = "0101001010";
+    int x = 2;
+    // cout << "code runs";
+    int cost = s.minOperations(s1, s2, x);
+    cout << "\n\n\n\ncost = " << cost << "\n\n\n\n";
+    return 0;
 }
