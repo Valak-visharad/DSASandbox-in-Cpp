06b74fa01431394c748403e2714408ea7de6c67c
Valak <59192647+Valak-visharad@users.noreply.github.com>
2023-11-09 21:53:53 +0530

    Update solution.cpp

--- 2896. Apply Operations to Make Two Strings Equal/solution.cpp
+++ 2896. Apply Operations to Make Two Strings Equal/solution.cpp
@@ -1,2 +1,47 @@
 // 2896. Apply Operations to Make Two Strings Equal
-// 
+
+class Solution {
+public:
+    int minOperations(string s1, string s2, int x) {
+        int s1_one = 0, s2_one = 0;
+        int n = sizeof(s1);
+        int prevIndex = 0, accounted = 0;
+        int cost = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s1[i] == '1')
+                s1_one++;
+            if (s2[i] == '1')
+                s2_one++;
+            
+            if (s1[i] != s2[i])
+            {
+                if (accounted)
+                {
+                    prevIndex = i;
+                    accounted = 0;
+                }
+                else
+                {
+                    cost += (i - prevIndex == 1 ? 1 : x);
+                    accounted = 1;
+                }
+            }
+        }
+        if (s1_one == s2_one)
+            return cost;
+
+        cout << s1_one << s2_one; // DEBUG CODE
+
+        return -1;
+    }
+};
+
+
+
+int main()
+{
+    Solution s;
+    s.minOperations()
+}
